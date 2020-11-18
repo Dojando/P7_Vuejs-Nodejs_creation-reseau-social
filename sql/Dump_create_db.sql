92a02dea855e55227ffd734ce0ce54417568f306
Dojando <wadja.b@hotmail.fr>
2020-11-18 14:13:37 +0100

    changement mineur script de la db

--- sql/Dump_create_db.sql
+++ sql/Dump_create_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `groupomaniadb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `groupomaniadb`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: groupomaniadb
@@ -85,4 +87,4 @@ CREATE TABLE `utilisateurs` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-17 23:42:48
+-- Dump completed on 2020-11-18 14:00:11
